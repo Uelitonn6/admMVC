1a27802d3a3dc51881b3b0d4ee89746fc13eda45
uelitonn6 <uelitonn6@gmail.com>
2024-08-31 17:33:53 -0300

    Pegando a url

--- app/adms/Controllers/Services/PageController.php
+++ app/adms/Controllers/Services/PageController.php
@@ -8,10 +8,26 @@ namespace App\adms\Controllers\Services;
  * @author Ueliton <uelitonn6@gmail.com>
  */
 class PageController
-{
+{   
+    
+    //Receber a URL do .htaccess
+    private string $url;
 
+    //Recebe a URL do .htaccess
     public function __construct()
     {
-       echo 'Carregar a pagina';
+       echo 'Carregar a pagina <br><br>';
+
+       // Verificar se vem valor na variavel url enviada pelo .htaccess
+       if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))) 
+       {
+
+            //Receber o valor da variavel url enviada pelo .htaccess
+            $this->url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
+
+            echo "Acessando o endereco: {$this->url} <br><br>";
+       } else {
+            echo "Acessar a pagina principal.<br><br>";
+       }
     }
 }
